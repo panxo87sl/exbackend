25dba33d3a0b5de9daad9795d46fb49d026fc3e2
panxo87sl <savage87.sl@gmail.com>
2025-07-11 01:29:05 -0400

    handlebars config & inicio

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import express from "express";
 //routes
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+//managers
+import ProductManager from "./managers/ProductManager.js";
 //plantillas handlebars
-import hbs from "express-handlebars";
+import { engine } from "express-handlebars";
 
 //instancia y configuracion server
 const server = express(); //instancia principal
@@ -13,6 +15,14 @@ const port = "8080"; //variable puerto
 //se montan los routers
 server.use("/api/products", productsRouter); //implementacion router
 server.use("/api/carts", cartsRouter); //implementacion router
+//handlebars
+server.get("/", async (request, response) => {
+  server.engine("handlebars", engine());
+  server.set("view engine", "handlebars");
+  server.set("views", import.meta.dirname + "/views");
+  const products = await ProductManager.getAll();
+  response.render("home", { layout: false, products });
+});
 
 server.listen(port, () => {
   console.log(`servidor escuchando en http://localhost:${port}`);
